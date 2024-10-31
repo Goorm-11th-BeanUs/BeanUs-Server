27e0df12b929a0bb141f17dfc91ddaaef86101aa
Keonyoung Shim <kyshim9004@gmail.com>
2024-10-31 10:05:45 +0900

    refactor: mock main01 feature, return history_id

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from datetime import datetime, timezone, timedelta
 from fastapi import FastAPI
 from pydantic import BaseModel, AwareDatetime
 
+import random
+
 app = FastAPI()
 
 
@@ -10,6 +12,7 @@ class CoffeeRequest(BaseModel):
     cafe_id: int
     request_datetime: AwareDatetime
     amount: int
+
 
 class CoffeeHistory(BaseModel):
     history_id: int
@@ -26,13 +29,15 @@ async def health_check():
 @app.post("/api/coffee/collect", status_code=201)
 async def coffee_collect(coffee_request: CoffeeRequest) -> dict:
     print(coffee_request)
-    return {"msg": "created"}
+
+    return {"history_id": random.randint(1, 100), "msg": "created"}
 
 
 @app.get("/api/coffee/history", status_code=200)
 async def coffee_collect(cafe_id: int) -> dict:
-    CoffeeHistory01 = CoffeeHistory(history_id=1, cafe_id=cafe_id, request_datetime=datetime.now(tz=timezone(timedelta(hours=9))), amount=500)
-    CoffeeHistory02 = CoffeeHistory(history_id=2, cafe_id=cafe_id, request_datetime=datetime.now(tz=timezone(timedelta(hours=9))), amount=1000)
+    CoffeeHistory01 = CoffeeHistory(history_id=1, cafe_id=cafe_id,
+                                    request_datetime=datetime.now(tz=timezone(timedelta(hours=9))), amount=500)
+    CoffeeHistory02 = CoffeeHistory(history_id=2, cafe_id=cafe_id,
+                                    request_datetime=datetime.now(tz=timezone(timedelta(hours=9))), amount=1000)
 
     return {"history": [CoffeeHistory01, CoffeeHistory02]}
-
