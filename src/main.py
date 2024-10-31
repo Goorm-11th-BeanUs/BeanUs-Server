4f3c4d5cdbc953d46944b62ab081500a3b3887eb
Keonyoung Shim <kyshim9004@gmail.com>
2024-10-31 17:38:10 +0900

    add mock api of main

--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@ from datetime import datetime, timezone, timedelta
 
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel, AwareDatetime
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
 from typing import List
 
 import random
 
-from src.config import database
-from src.model import User
+# from src.config import database
+# from src.model import User
 
 app = FastAPI()
 
-database.Base.metadata.create_all(bind=database.engine)
+# database.Base.metadata.create_all(bind=database.engine)
 
 
 class UserRead(BaseModel):
@@ -30,34 +30,44 @@ class UserRead(BaseModel):
 
 
 # GET 요청으로 모든 유저 정보 가져오기
-@app.get("/api/users/", response_model=List[UserRead])
-def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
-    print("read_users_hi")
-    users = db.query(User).offset(skip).limit(limit).all()
-    return users
+# @app.get("/api/users/", response_model=List[UserRead])
+# def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
+#     print("read_users_hi")
+#     users = db.query(User).offset(skip).limit(limit).all()
+#     return users
+#
+#
+# # 특정 유저 정보 가져오기
+# @app.get("/api/users/{user_id}", response_model=UserRead)
+# def read_user(user_id: int, db: Session = Depends(database.get_db)):
+#     print("read_user_hi")
+#     user = db.query(User).filter(User.id == user_id).first()
+#     if user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return user
 
 
-# 특정 유저 정보 가져오기
-@app.get("/api/users/{user_id}", response_model=UserRead)
-def read_user(user_id: int, db: Session = Depends(database.get_db)):
-    print("read_user_hi")
-    user = db.query(User).filter(User.id == user_id).first()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+class CollectDays(BaseModel):
+    weekday: int
+    time: str
 
 
 class CoffeeRequest(BaseModel):
     cafe_id: int
-    request_datetime: AwareDatetime
+    collect_days: List[CollectDays]
     amount: int
 
 
 class CoffeeHistory(BaseModel):
     history_id: int
-    cafe_id: int
-    request_datetime: AwareDatetime
+    client_name: str
+    time: AwareDatetime
+    state: str
     amount: int
+
+
+class CancelCoffee(BaseModel):
+    history_id: int
 
 
 @app.get("/health")
@@ -67,16 +77,30 @@ async def health_check():
 
 @app.post("/api/coffee/collect", status_code=201)
 async def coffee_collect(coffee_request: CoffeeRequest) -> dict:
-    print(coffee_request)
-
-    return {"history_id": random.randint(1, 100), "msg": "created"}
+    return {"history_id": random.randint(1, 100)}
 
 
-@app.get("/api/coffee/history", status_code=200)
-async def coffee_collect(cafe_id: int) -> dict:
-    coffeeHistory01 = CoffeeHistory(history_id=1, cafe_id=cafe_id,
-                                    request_datetime=datetime.now(tz=timezone(timedelta(hours=9))), amount=500 * random.randint(1, 10))
-    coffeeHistory02 = CoffeeHistory(history_id=2, cafe_id=cafe_id,
-                                    request_datetime=datetime.now(tz=timezone(timedelta(hours=9))), amount=1000 * random.randint(1, 10))
+@app.post("/api/coffee/cancel", status_code=204)
+async def coffee_cancel(cancel_coffee: CancelCoffee):
+    return None
 
-    return {"history": [coffeeHistory01, coffeeHistory02]}
+
+@app.get("/api/coffee/history/{cafe_id}", status_code=200)
+async def coffee_history(cafe_id: int) -> dict:
+    coffeeHistory01 = CoffeeHistory(history_id=1, client_name="abc학교",
+                                    time=datetime.now(tz=timezone(timedelta(hours=9))), state="READY"
+                                    , amount=10 * random.randint(1, 10))
+    coffeeHistory02 = CoffeeHistory(history_id=2, client_name="123시청",
+                                    time=datetime.now(tz=timezone(timedelta(hours=9))), state="QUEUED"
+                                    , amount=10 * random.randint(1, 10))
+    coffeeHistory03 = CoffeeHistory(history_id=3, client_name="ㄱㄴㄷ회사",
+                                    time=datetime.now(tz=timezone(timedelta(hours=9))), state="COMPLETED"
+                                    , amount=10 * random.randint(1, 10))
+    coffeeHistory04 = CoffeeHistory(history_id=5, client_name="ㄱㄴㄷ회사2",
+                                    time=datetime.now(tz=timezone(timedelta(hours=9))), state="COMPLETED"
+                                    , amount=10 * random.randint(1, 10))
+
+    res = {"histories": [coffeeHistory01, coffeeHistory02, coffeeHistory03, coffeeHistory04]}
+
+    print(res)
+    return res
--- src/model/User.py
+++ src/model/User.py
@@ -1,17 +1,17 @@
-from datetime import datetime, timedelta, timezone
-from sqlalchemy import Column, Integer, String, DateTime
-from src.config.database import Base
-
-
-class User(Base):
-    __tablename__ = "user"
-
-    id = Column(Integer, primary_key=True, index=True)
-    token = Column(Integer, primary_key=False, index=False)
-    name = Column(String(100))
-    address = Column(String(100))
-    phone_number = Column(String(100))
-    role = Column(String(100))
-    created_at = Column(DateTime, default=datetime.now(timezone(timedelta(hours=9))))
-    updated_at = Column(DateTime, default=datetime.now(timezone(timedelta(hours=9))))
+# from datetime import datetime, timedelta, timezone
+# from sqlalchemy import Column, Integer, String, DateTime
+# from src.config.database import Base
+#
+#
+# class User(Base):
+#     __tablename__ = "user"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     token = Column(Integer, primary_key=False, index=False)
+#     name = Column(String(100))
+#     address = Column(String(100))
+#     phone_number = Column(String(100))
+#     role = Column(String(100))
+#     created_at = Column(DateTime, default=datetime.now(timezone(timedelta(hours=9))))
+#     updated_at = Column(DateTime, default=datetime.now(timezone(timedelta(hours=9))))
 
